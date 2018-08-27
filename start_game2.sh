bfd64bf32dee0744edf74db9d54787cc39107845
Chongyang Bai <cy2@hyperion.stanford.edu>
2018-08-27 11:58:29 -0700

    add bash file

---
+++ start_game2.sh
@@ -0,0 +1,10 @@
+python game_split_CoNN.py 2 0 &
+python game_split_CoNN.py 2 1 &
+python game_split_CoNN.py 2 2 &
+python game_split_CoNN.py 2 3 &
+python game_split_CoNN.py 2 4 &
+python game_split_CoNN.py 2 5 &
+python game_split_CoNN.py 2 6 &
+python game_split_CoNN.py 2 7 &
+python game_split_CoNN.py 2 8 &
+python game_split_CoNN.py 2 9 &
