1ac366b03e87d0f49b4b4e1456fe690303d65cb0
srijankr <srijan@stanford.edu>
2018-08-27 13:00:40 -0700

    minor

--- start_game2.sh
+++ start_game2.sh
@@ -5,7 +5,7 @@ split=0
 while [ $split -lt 9 ]
 do
     echo $game $split
-    #python game_split_CoNN.py $game $split &
+    python game_split_CoNN.py --game_id=$game --split_id=$split --epochs=50 --time_granularity=10 --layer=3 &
     sleep 1
     (( split++ ))
 done
